41ae9d637e11a3f7b8b1cfe688997875a7c7978f
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-08-28 23:12:46 -0400

    activate listener

--- service-worker.js
+++ service-worker.js
@@ -11,8 +11,28 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
+            console.log('Installing cache: ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE);
         })
     )
 })
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheList.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheList.indexOf(key) === -1) {
+                        console.log('Deleting cache: ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
